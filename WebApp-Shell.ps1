6543d9b12f8b2288035755ef877ec7610fed4a70
Shaked Wiessman <shakedwe2@gmail.com>
2025-08-19 20:36:06 +0000

    Update WebApp-Shell.ps1

--- WebApp-Shell.ps1
+++ WebApp-Shell.ps1
@@ -63,8 +63,8 @@ function Show-ClientLoginGui {
         </Grid.ColumnDefinitions>
 
         <StackPanel Grid.Column="1" Margin="14,0,0,0" VerticalAlignment="Center">
-          <TextBlock Text="Vaulter - Key Vault Enumeration" Foreground="#f8fafc" FontSize="22" FontWeight="Bold"/>
-          <TextBlock Text="By ART - AB-inBev 2025" Foreground="#E0E7FF" FontSize="13"/>
+          <TextBlock Text="WebApp-Shell Interactive Shell with WebApp" Foreground="#f8fafc" FontSize="22" FontWeight="Bold"/>
+           <TextBlock Text="By @ShkudW - https://github.com/ShkudW/ARM-Enum " Foreground="#E0E7FF" FontSize="13"/>
         </StackPanel>
       </Grid>
     </Border>
